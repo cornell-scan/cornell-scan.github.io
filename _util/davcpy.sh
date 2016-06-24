ef646a468e694c11eff0314993ad4901243706ed
David Bindel <bindel@cs.cornell.edu>
2016-06-24 00:38:22 -0400

    Update to push to CSE web pages.

---
+++ _util/davcpy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd /home/bindel/work/scan-seminar/
+jekyll build
+cd _site
+rc=$(mktemp)
+
+{
+    echo "open https://static-webdav.kproxy.cornell.edu/cse" 
+    find "." -type d | xargs -I{} echo 'mkcol scan/'{}
+    find "." -type f \
+    -exec echo 'cd scan/'$(dirname {}) \; \
+    -exec echo 'lcd '$(dirname {}) \; \
+    -exec echo 'mput '$(basename {}) \; \
+    -exec echo 'cd -' \; \
+    -exec echo 'lcd /home/bindel/work/scan-seminar/_site' \;
+    echo "quit";
+} > "$rc";
+
+cadaver -r "$rc" "$@" 
+rm -f "$rc"
+echo "Finished upload"
